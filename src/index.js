35d2179335282e0b9cea22de0caf90f6f18b7ba7
n0g006r <nikhil.garg@walmart.com>
2020-01-07 17:49:24 +0530

    Installed material ui and added basic grid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 class LoginPage extends React.Component{
   render(){
     return (
-      <div> hello world</div>
+      <Grid
+        container
+        direction="row"
+        >
+        <Grid item xs={4}>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
