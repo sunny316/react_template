72054cb531f2007c060f5e869f0510ee4c5699c4
Paras <Paras.Taneja@walmart.com>
2020-01-08 20:12:00 +0530

    login and google

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
+import Icon from '@material-ui/icons';
 
 class LoginPage extends React.Component{
   render(){
     return (
       <Grid
         container
-        direction="row"
+        direction="column"
+        justify="center"
+        alignItems="center"
         >
-        <Grid item xs={4}>
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
+        <Fab variant="extended" color="primary" style={{backgroundColor: '#41e590', width: '250px', marginBottom: '20px' }}>
+          Navigate
+        </Fab>
+        <Grid
+         container
+         direction="row"
+         justify="center"
+         alignItems="center"
+         style={{ marginBottom: '20px' }}
+        >
+          <div style={{ height: '1px', width: '115px', backgroundColor: '#6c6c6c' }} />
+          <span style={{ color: '#6c6c6c'}}>OR</span>
+          <div style={{ height: '1px', width: '115px', backgroundColor: '#6c6c6c' }} />
         </Grid>
-        <Grid item xs={4}>
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-        </Grid>
+        <Fab variant="extended" color="primary" style={{backgroundColor: '#fff', width: '250px', color: '#000' }}>
+          Login with Google
+        </Fab>
       </Grid>
     );
   }
