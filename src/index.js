b35b58cc2e72b0bd8c854785c00e12d48dfa7d0e
n0g006r <nikhil.garg@walmart.com>
2020-01-12 15:20:34 +0530

    Responsive font and labels

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+import { createMuiTheme, ThemeProvider, responsiveFontSizes } from '@material-ui/core/styles';
 import { Grid, Radio, Fab, OutlinedInput, FormControl, InputLabel, InputAdornment, IconButton, withStyles, useTheme, useMediaQuery } from '@material-ui/core';
 import { Visibility, VisibilityOff, Favorite } from '@material-ui/icons';
 import AppBar from '@material-ui/core/AppBar';
@@ -50,21 +51,27 @@ const styles = theme => ({
     minHeight: "56px",
     [theme.breakpoints.up(1920)]: {
       minHeight: "64px",
+      fontSize: "0.875rem"
     },
     [theme.breakpoints.up(2560)]: {
       minHeight: "80px",
+      fontSize: "1.05rem"
     },
     [theme.breakpoints.up(3200)]: {
       minHeight: "96px",
+      fontSize: "1.15rem"
     },
     [theme.breakpoints.up(3840)]: {
       minHeight: "112px",
+      fontSize: "1.25rem"
     },
     [theme.breakpoints.up(4600)]: {
       minHeight: "128px",
+      fontSize: "1.35rem"
     },
     [theme.breakpoints.up(5200)]: {
       minHeight: "146px",
+      fontSize: "1.45rem"
     },
   },
   header: {
@@ -76,7 +83,6 @@ const styles = theme => ({
     color: 'black'
   },
   signin: {
-    fontSize: "2.8125rem",
     fontWeight: 700,
     fontStretch: "condensed",
     fontStyle: "normal",
@@ -84,40 +90,44 @@ const styles = theme => ({
     color: "#656565"
   },
   subtitle1: {
-    fontSize: "1.125rem",
     fontWeight: 300,
     fontStretch: "normal",
     fontStyle: "normal",
-    letterSpacing: ".36px",
     textAlign: "center",
     color: "#656565",
-    fontFamily: "helveticaNeueLight"
+    // fontFamily: "helveticaNeueLight"
   },
   formgrid: {
     marginTop: "2%"
   },
   rememberText: {
     marginRight: "16px",
-    fontSize: 16,
     fontFamily: 'helveticaNeueLight',
     fontWeight: 400
   },
   forgotText: {
     color: '#696969',
-    fontSize: 16,
     fontFamily: 'helveticaNeueLight',
     fontWeight: 400
   },
   noAccountText: {
     color: '#696969',
-    fontSize: 16,
     fontFamily: 'helveticaNeueLight',
     fontWeight: 400,
   },
   dividerStyle: {
     height: '1px',
-    width: '115px',
+    width: '8%',
     backgroundColor: '#6c6c6c',
+    [theme.breakpoints.down(1920)]: {
+      width: "115px",
+    },
+  },
+  emaillabel: {
+    transform:"translate(50%,155%) scale(1)"
+  },
+  passlabel: {
+    transform:"translate(30%,155%) scale(1)"
   },
   footerContainer: {
     position: "absolute",
@@ -126,55 +136,63 @@ const styles = theme => ({
   },
   footerText: {
     color: '#656565',
-    lineHeight: 1.17,
-    fontSize: ".8rem",
     flexGrow: 1
   },
   footerLoveIcon: {
     color: 'rgb(234, 84, 85)', 
-    fontSize: ".8rem",
   },
   footerPrivacyText: {
     color: '#C8C8C8',
-    fontSize: "1rem",
     marginRight: "20px",
-    lineHeight: 1.2,
   }
 });
 
 const HOC = (LoginPage) => {
   return (props) => {
-    const theme = useTheme();
+    // const theme = useTheme();
+    // theme.spacing.unit
     let spacing = 2;
     let borderRadius = "30px";
+    let labelWidth = 18;
     const media1920 = useMediaQuery(theme.breakpoints.down(1920));
     const media2560 =  useMediaQuery(theme.breakpoints.between(1920, 2560));
-    const media3200 = useMediaQuery(theme.breakpoints.between(3200, 3840));
-    const media3840 = useMediaQuery(theme.breakpoints.between(3840, 4600));
-    const media4600 = useMediaQuery(theme.breakpoints.between(4600, 5200));
-    const media5200 = useMediaQuery(theme.breakpoints.up(5200));
+    const media3200 =  useMediaQuery(theme.breakpoints.between(2560, 3200));
+    const media3840 = useMediaQuery(theme.breakpoints.between(3200, 3840));
+    const media4600 = useMediaQuery(theme.breakpoints.between(3840, 4600));
+    const media5200 = useMediaQuery(theme.breakpoints.between(4600, 5200));
+    const media5200up = useMediaQuery(theme.breakpoints.up(5200));
     if(media1920){
       spacing = 2; // 4px multiplier up and down space so 16px(2*4 + 2*4) spacing
+      labelWidth = 30;
       borderRadius = "30px";
     }else if(media2560){
       spacing = 3;
-      borderRadius = "40px";
+      labelWidth = 50;
+      borderRadius = "32px";
     }else if(media3200) {
       spacing = 4;
-      borderRadius = "48px";
+      labelWidth = 65;
+      borderRadius = "40px";
     }else if(media3840) {
       spacing = 6;
-      borderRadius = "56px";
+      labelWidth = 74;
+      borderRadius = "48px";
     }else if(media4600) {
       spacing = 8;
-      borderRadius = "64px";
+      labelWidth = 80;
+      borderRadius = "56px";
     }else if(media5200) {
       spacing = 10;
+      labelWidth = 84;
+      borderRadius = "64px";
+    }else if(media5200up) {
+      spacing = 10;
+      labelWidth = 86;
       borderRadius = "73px";
     }
     console.log(spacing);
     return(
-       <LoginPage {...props} spacing={spacing} borderRadius={borderRadius} />
+       <LoginPage {...props} spacing={spacing} borderRadius={borderRadius} labelWidth={labelWidth} />
     )
   }
 }
@@ -197,14 +215,17 @@ class LoginPage extends React.Component {
     this.setState({ showPassword: !this.state.showPassword });
   };
   render() {
-    const { classes, spacing, borderRadius } = this.props;
+    const { classes, spacing, borderRadius, labelWidth } = this.props;
+    console.log(labelWidth);
     return (
       <Fragment>
         <AppBar position="static" className={classes.header}>
           <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              Sample App
-            </Typography>
+            <ThemeProvider theme={theme}> 
+              <Typography variant="h6" className={classes.title}>
+                Sample App
+              </Typography>
+            </ThemeProvider>
             <IconButton edge="start" className={classes.menuButton} color="primary" aria-label="menu">
               <MenuIcon />
             </IconButton>
@@ -219,25 +240,35 @@ class LoginPage extends React.Component {
           className={classes.formgrid}
         >
           <Grid item>
-            <Typography variant="h3" className={classes.signin}>
-              SIGN IN
-            </Typography>
+            <ThemeProvider theme={theme}>
+              <Typography variant="h3" className={classes.signin}>
+                SIGN IN
+              </Typography>
+            </ThemeProvider>
           </Grid>
           <Grid item>
-            <p className={classes.subtitle1}>
-              Enter your email and password to login
-            </p>
+            <ThemeProvider theme={theme}>
+              <Typography variant="subtitle1" className={classes.subtitle1}>
+                Enter your email and password to login
+              </Typography>
+            </ThemeProvider>
           </Grid>
           <Grid item className={classes.formwidth}>
             <FormControl variant="outlined" className={classes.insideFormWidth}>
-              <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+              <InputLabel htmlFor="outlined-adornment-email" className={classes.emaillabel}>
+                <ThemeProvider theme={theme}>
+                  <Typography variant="body2">
+                    Email
+                  </Typography>
+                </ThemeProvider>
+              </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-email"
                 type='text'
                 value={this.state.email}
                 style={{ borderRadius: borderRadius }}
                 onChange={(e) => this.handleChange(e, 'email')}
-                labelWidth={70}
+                labelWidth={labelWidth}
                 required={true}
                 className={classes.inputBox}
               />
@@ -245,7 +276,13 @@ class LoginPage extends React.Component {
           </Grid>
           <Grid item className={classes.formwidth}>
             <FormControl variant="outlined" className={classes.insideFormWidth}>
-              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <InputLabel htmlFor="outlined-adornment-password" className={classes.passlabel}>
+                <ThemeProvider theme={theme}>
+                  <Typography variant="body2">
+                    Password
+                  </Typography>
+                </ThemeProvider>
+                </InputLabel>
               <OutlinedInput
                 id="outlined-adornment-password"
                 type={this.state.showPassword ? 'text' : 'password'}
@@ -263,7 +300,7 @@ class LoginPage extends React.Component {
                     </IconButton>
                   </InputAdornment>
                 }
-                labelWidth={70}
+                labelWidth={labelWidth*1.6}
                 required={true}
               />
             </FormControl>
@@ -275,8 +312,14 @@ class LoginPage extends React.Component {
             justify="center"
           >
             <Radio style={{ color: 'rgb(122, 201, 255)' }} />
-            <span className={classes.rememberText}>Remember me</span>
-            <span className={classes.forgotText}>Forgot password ?</span>
+            <ThemeProvider theme={theme}>
+              <Typography variant="subtitle1" className={classes.rememberText}>
+                Remember me
+              </Typography>
+              <Typography variant="subtitle1" className={classes.forgotText}>
+                Forgot password ?
+              </Typography>
+            </ThemeProvider>
           </Grid>
           <Grid item className={classes.formwidth}>
             <Fab variant="extended" color="primary" className={classes.insideFormWidth} style={{ backgroundColor: '#41e590', borderRadius: borderRadius }}>
@@ -288,7 +331,11 @@ class LoginPage extends React.Component {
             justify="center"
             alignItems="center">
             <div className={classes.dividerStyle} />
-            <span style={{ color: '#6c6c6c' }}>OR</span>
+            <ThemeProvider theme={theme}>
+              <Typography variant="subtitle1" style={{ color: '#6c6c6c' }}>
+                  OR  
+              </Typography>
+            </ThemeProvider>
             <div className={classes.dividerStyle} />
           </Grid>
           <Grid item className={classes.formwidth}>
@@ -297,9 +344,11 @@ class LoginPage extends React.Component {
             </Fab>
           </Grid>
           <Grid item>
-            <div className={classes.noAccountText}>
+            <ThemeProvider theme={theme}>
+              <Typography variant="subtitle1" className={classes.noAccountText}>
               Don't have an account? Sign Up
-            </div>
+              </Typography>
+            </ThemeProvider>
           </Grid>
           <Grid container item
             direction="row"
@@ -307,21 +356,62 @@ class LoginPage extends React.Component {
             alignContent="flex-end"
             className={classes.footerContainer}
           >
-            <div className={classes.footerText}>
+            {/* <div className={classes.footerText}>
               <span>Made with </span>
-              <Favorite className={classes.footerLoveIcon} />
+              
               <span> in Chicago</span>
-            </div>
-            <span  className={classes.footerPrivacyText}>Privacy Policy</span>
-            <span  className={classes.footerPrivacyText}>Term of use</span>
-            <span  className={classes.footerPrivacyText}>Helps</span>
+            </div> */}
+            <ThemeProvider theme={theme}>
+              <Typography variant="subtitle1" className={classes.footerText}>
+                  Made with <Favorite className={classes.footerLoveIcon} /> in Chicago
+              </Typography>
+              <Typography variant="subtitle1" className={classes.footerPrivacyText}>
+                  Privacy Policy
+              </Typography>
+              <Typography variant="subtitle1" className={classes.footerPrivacyText}>
+                  Terms of use
+              </Typography>
+              <Typography variant="subtitle1" className={classes.footerPrivacyText}>
+                  Help
+              </Typography>
+            </ThemeProvider>
           </Grid>
         </Grid>
       </Fragment>
     );
   }
 }
-
+let theme = createMuiTheme();
+function scalefontsize(basesize){
+  console.log(basesize);
+  
+  return {
+    fontSize: basesize.toString() + 'rem',
+    [theme.breakpoints.up('xl')]: {
+      fontSize: (basesize*1.2).toString() + 'rem',
+    },
+    [theme.breakpoints.up(2560)]: {
+      fontSize: (basesize*1.45).toString() + 'rem',
+    },
+    [theme.breakpoints.up(3200)]: {
+      fontSize: (basesize*1.65).toString() + 'rem',
+    },
+    [theme.breakpoints.up(3840)]: {
+      fontSize: (basesize*1.85).toString() + 'rem',
+    },
+    [theme.breakpoints.up(4600)]: {
+      fontSize: (basesize*2.05).toString() + 'rem',
+    },
+    [theme.breakpoints.up(5200)]: {
+      fontSize: (basesize*2.25).toString() + 'rem',
+    },
+  }
+}
+theme.typography.subtitle1 = scalefontsize(1.1);
+theme.typography.h3 = scalefontsize(1.8);
+theme.typography.h6 = scalefontsize(1.5);
+theme.typography.body2 = scalefontsize(0.9);
+// theme = responsiveFontSizes(theme);
 const Lender = withStyles(styles)(HOC(LoginPage));
 ReactDom.render(<Lender />, document.getElementById("app"));
 
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
         inline: true,
         port: '8080',
         allowedHosts: [
-            'localhost'
+            '0.0.0.0'
         ]
       },      
      plugins: [
